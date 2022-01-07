f8bed021d02f2c135554f9c05f6ff546e7cdc7aa
dinshows15 <97285759+dinshows15@users.noreply.github.com>
2022-01-07 16:24:12 +0530

    Update Reverse_str.py

--- Reverse_str.py
+++ Reverse_str.py
@@ -1,7 +1,18 @@
-def reverse(s):
+def reverse_1(s):
     str =''
     for i in s:
         str= i +str
     return str
+
+def reverse_2(string):
+    string = string[::-1]
+    return string
+
+def reverse_3(string):
+    string = "".join(reversed(string))
+    return string
+
 s= input('Enter Original String: ')
-print(reverse(s))
+print(reverse_1(s))
+print(reverse_2(s))
+print(reverse_3(s))
